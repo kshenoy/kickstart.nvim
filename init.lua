e515135731aa99c8290a01660907581216076d26
Kartik Shenoy <theresmorethanoneway@gmail.com>
2023-11-11 05:16:16 -0800

    Moved everything from the custom dir to the top-level

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 -- Define leader and some keymaps ahead of time.
 -- This needs to be the first thing so that everything else can make use of it
-require('custom.keymaps.setup')
+require('keymaps.setup')
 
 -- Load kickstart
 require('kickstart')
 
 -- Now load the rest of my configuration.
--- These are all stored in the lua/custom folder to keep it modular and distinct from kickstart's config
--- Note that the package manager (lazy.nvim) automatically loads all the plugins specified in custom/plugins
+-- These are all stored in the lua folder to keep it modular and distinct from kickstart's config
+-- Note that the package manager (lazy.nvim) automatically loads all the plugins specified in plugins
 -- So I don't have to specify that explicitly
-require('custom.options')
-require('custom.keymaps')
+require('options')
+require('keymaps')
